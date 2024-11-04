b215f7fec9296eee8e51c57561a023cae795efa7
Davi Santana <davi.santana@renova.net.br>
2024-11-04 15:31:19 -0300

    Atividade 2

---
+++ Atividades/src/MinhasAtividades2/Atividade2.java
@@ -0,0 +1,39 @@
+package MinhasAtividades2;
+
+
+
+
+import java.util.Scanner;
+
+
+public class Atividade2 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+         int A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, R = 0; 
+         
+         
+         System.out.println("Expressão: (a+b) . (c-d) . (e+f)");
+         System.out.println("          -----------------------");
+         System.out.println("                      2");
+	         System.out.println("Insira A");
+                 A = teclado.nextInt();
+                 System.out.println("Insira B");
+                     B = teclado.nextInt();
+                    System.out.println("Insira C");
+                        C = teclado.nextInt();
+                           System.out.println("Insira D");
+                               D = teclado.nextInt();
+                               System.out.println("Insira E");
+                                   E = teclado.nextInt();
+                                   System.out.println("Insira F");
+                                       F = teclado.nextInt();
+        R = ((A+B)*(C-D)*(E+F)/2); 
+                                       
+	    System.out.println(String.format("Eu sou FERA nas continhas e o resultado é %d", R)); 
+	
+                                       
+	}
+
+
+}
