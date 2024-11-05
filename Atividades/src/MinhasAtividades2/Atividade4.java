2f5128757193c05a92b11d8eec23626a76ef9735
Davi Santana <davi.santana@renova.net.br>
2024-11-05 14:04:25 -0300

    Adição da atividade 4

---
+++ Atividades/src/MinhasAtividades2/Atividade4.java
@@ -0,0 +1,18 @@
+package MinhasAtividades2;
+
+import java.util.Scanner;
+
+public class Atividade4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int titas;
+	Scanner teclado = new Scanner(System.in);	
+
+	    System.out.println("Insira o número de titãs: ");
+	    titas = teclado.nextInt();
+	   int tropas = (titas-20)/5; 
+	    System.out.println("O número de soldados necessários será:" + tropas);
+	}
+
+}
