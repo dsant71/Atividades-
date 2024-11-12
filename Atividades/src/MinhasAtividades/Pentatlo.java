fdd5cd68e4ea4c235a7f11ddb2193c2db8c7a61c
Davi Santana <davi.santana@renova.net.br>
2024-11-12 15:55:08 -0300

    Adição da atividade Pentatlo.

---
+++ Atividades/src/MinhasAtividades/Pentatlo.java
@@ -0,0 +1,37 @@
+package MinhasAtividades;
+
+import java.util.Scanner;
+
+public class Pentatlo {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+        int competidor;
+        float n1, n2, n3, n4, n5, media;
+    System.out.println("Insira o número de inscrição do competidor");
+	competidor = teclado.nextInt();
+		
+		if (competidor > 1000 || competidor < 0) {
+			System.out.println("Numero de inscrição inválido");
+			} else {
+	
+		System.out.println("Insira as 5 notas do competidor");
+		n1 = teclado.nextFloat();
+		n2 = teclado.nextFloat();
+		n3 = teclado.nextFloat();
+		n4 = teclado.nextFloat();
+		n5 = teclado.nextFloat();
+		
+		if (n1 > 10 || n2 > 10 || n3 > 10 || n4 > 10|| n5 > 10 ) {
+			System.out.println("Nota inválida");
+		
+		} else {
+	
+		media = (n1 + n2 + n3 + n4 + n5)/5;
+		
+			System.out.println("A média final do competidor " + competidor + " foi: " + media);
+		}
+	       }
+	}
+
+}
