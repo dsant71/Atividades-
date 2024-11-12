04551a531bc0b49899e5c5ead5bfa266a6aa2f9d
Davi Santana <davi.santana@renova.net.br>
2024-11-12 16:07:12 -0300

    Correções na atividade Pentatlo

--- Atividades/src/MinhasAtividades/Pentatlo.java
+++ Atividades/src/MinhasAtividades/Pentatlo.java
@@ -8,29 +8,35 @@ public class Pentatlo {
 		Scanner teclado = new Scanner (System.in);
         int competidor;
         float n1, n2, n3, n4, n5, media;
-    System.out.println("Insira o número de inscrição do competidor");
-	competidor = teclado.nextInt();
-		
-		if (competidor > 1000 || competidor < 0) {
-			System.out.println("Numero de inscrição inválido");
-			} else {
+    
+        	System.out.println("Insira o número de inscrição do competidor");
+        	competidor = teclado.nextInt();
+        		
+        		if (competidor > 1000 || competidor < 0) {
+        			System.out.println("Numero de inscrição inválido");
+        	  
+        	  } else {
 	
-		System.out.println("Insira as 5 notas do competidor");
-		n1 = teclado.nextFloat();
-		n2 = teclado.nextFloat();
-		n3 = teclado.nextFloat();
-		n4 = teclado.nextFloat();
-		n5 = teclado.nextFloat();
+        		   System.out.println("Insira as 5 notas do competidor");
+        		   	  n1 = teclado.nextFloat();
+        		   	  n2 = teclado.nextFloat();
+        		   	  n3 = teclado.nextFloat();
+        		   	  n4 = teclado.nextFloat();
+        		   	  n5 = teclado.nextFloat();
+        		   	  media = (n1 + n2 + n3 + n4 + n5)/5;
+        		
+        		if (n1 > 10 || n2 > 10 || n3 > 10 || n4 > 10|| n5 > 10 ) {
+        		  System.out.println("Nota inválida");
 		
-		if (n1 > 10 || n2 > 10 || n3 > 10 || n4 > 10|| n5 > 10 ) {
-			System.out.println("Nota inválida");
+        	  } else {
+        		  System.out.println("A média final do competidor " + competidor + " foi: " + media);
 		
-		} else {
-	
-		media = (n1 + n2 + n3 + n4 + n5)/5;
-		
-			System.out.println("A média final do competidor " + competidor + " foi: " + media);
-		}
+        	  
+       teclado.close();
+        	  
+        	  
+        	  
+        	  }
 	       }
 	}
 
