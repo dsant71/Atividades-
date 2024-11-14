05d4f9b3a3641fa6a8680b3f7e38264c33a02e6f
Davi Santana <davi.santana@renova.net.br>
2024-11-14 14:49:56 -0300

    Adição da atividade sobre a maior altura entre 3 pessoas

---
+++ Atividades/src/MinhasAtividades/Altura.java
@@ -0,0 +1,45 @@
+package MinhasAtividades;
+
+import java.util.Scanner;
+
+public class Altura {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		int altura1, altura2, altura3, maior = 0;
+		
+			System.out.println("Insira a altura em centímetros dos 3 amigos");
+			altura1 = teclado.nextInt();
+			altura2 = teclado.nextInt();
+			altura3 = teclado.nextInt();
+			
+					  if (altura1 < 100 || altura1 > 200) {
+						  System.out.println("Altura inválida.");
+			
+					} else if (altura2 < 100 || altura2 > 200) {
+						  System.out.println("Altura inválida.");
+					} else if (altura3 < 100 || altura3 > 200) {
+						  System.out.println("Altura inválida.");
+					
+					} else {	  
+						
+						if (altura1 > maior) {
+							maior = altura1;
+						}
+						if (altura2 > maior) {
+							maior = altura2;
+						}	
+						if (altura3 > maior) {
+							maior = altura3;
+						}
+			
+		    System.out.println("O amigo mais alto possui " + maior + "cm de altura");
+			
+			
+			
+			
+		}
+
+	}
+
+}
