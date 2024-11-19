3a46f29de8c64dff14b438384ac035e0148d44a1
Davi Santana <davi.santana@renova.net.br>
2024-11-19 16:55:26 -0300

    Atualização em duas atividades do pacote

--- Atividades/src/MinhasAtividades/DesafioTatico.java
+++ Atividades/src/MinhasAtividades/DesafioTatico.java
@@ -1,10 +1,39 @@
 package MinhasAtividades;
 
+import java.util.Scanner;
+
 public class DesafioTatico {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        
+        int P = teclado.nextInt();
+        
+      
+        int S = teclado.nextInt();
+        
+       
+        for (int i = 0; i < P; i++) {
+            int somaAtaque = 0;
+            int somaDefesa = 0;
+            
+            
+            for (int j = 0; j < S; j++) {
+                int ataque = teclado.nextInt();  
+                int defesa = teclado.nextInt();   
+                
+                somaAtaque += ataque;  
+                somaDefesa += defesa;  
+            } 
+            
+         
+            System.out.println(somaAtaque + " " + somaDefesa);
+        }
+        
+       
+        teclado.close();
+    }
 }
+
+
--- Atividades/src/MinhasAtividades/EscolhaSeuCampeao.java
+++ Atividades/src/MinhasAtividades/EscolhaSeuCampeao.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EscolhaSeuCampeao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 Scanner teclado = new Scanner (System.in);
 
